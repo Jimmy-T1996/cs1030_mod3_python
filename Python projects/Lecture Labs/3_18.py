a5ee7d55333e9e0d9e1150f4a8a9cc3f4e959534
Jimmy-T1996 <j19.02t@Gmail.com>
2025-03-18 15:46:57 -0600

    Lecture Labs Folder created
    3/18 python lab for rock paper scissors created.

---
+++ Python projects/Lecture Labs/3_18.py
@@ -0,0 +1,30 @@
+
+import random
+
+computerSel = random.randint(1,3)
+
+
+playerSelection = []
+
+1 == rock
+2 == paper
+3 == scissors 
+
+playerSelection = input("type rock, paper, scissors")
+
+print("The player has selected {playerSelection}.")
+
+
+
+def rpsgame():
+    print(f"The computer picks {computerSel}")
+    if playerSelection == "rock" and computerSel == "rock" or playerSelection == "scissors" and computerSel == "scissors" or playerSelection == "paper" and computerSel == "paper":
+        print("its a draw!")
+    elif playerSelection == "rock" and computerSel == "scissors" or "paper" and computerSel == "rock" or "scissors" and computerSel == "paper":
+        print("Player 1 wins!")
+    else:
+        print("Computer wins!")
+
+rpsgame()
+
+    
