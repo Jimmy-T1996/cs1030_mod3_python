91d2d9f2d3aa15c07003140921d28e740b5006fc
Jimmy-T1996 <j19.02t@Gmail.com>
2025-04-07 17:17:21 -0600

    Tech Docs updated
    Pdf version of tech doc added. Python files were edited for screenshots in tech docs.

--- Python projects/Lecture Labs/forloops_inputs.py
+++ Python projects/Lecture Labs/forloops_inputs.py
@@ -7,22 +7,24 @@ Program: For Loops Practice
 Purpose: In Class work for understanding for loops.
 '''
 
-
+#This stores a list
 userinputs = []
 
-
+#This function saves inputs to the userinputs list.
 def input_function():
-
+    #While true starts a loop while boolean is implemented.
     while True:
+        #This takes inputs by the user.
         userinput = input("Please enter an input to save, type 'done' when you are finished:\n")
-
+        #This stops the loop.
         if userinput == "done":
             break
+        #This adds all userinput values that are not 'done' to the userinputs list.
         else:
             userinputs.append(userinput)
-    
+#This starts the function when program is started.    
 input_function()
 
 for userinput in userinputs:
-    print (userinput)
+    print(f"{userinput}")
 
